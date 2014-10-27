32110eaf5af6e7c09cd9702f6f3ef565e5b227dc
Jonathan Irwin <jirwin@cfa.harvard.edu>
2014-10-27 17:23:36 -0400

    Improved commenting of "testplanet".

--- python/testplanet.py
+++ python/testplanet.py
@@ -8,6 +8,11 @@
 #
 # The parameters are for Kepler-37b.
 #
+# This example uses the "phase offset" (really a mean anomaly
+# adjustment) parameter to adjust the time of the eclipse for
+# simplicity.  In practice we would presumably want to determine
+# the ephemeris (P and T0) would fit for those instead of
+# adjusting PHI0.
 
 import numpy
 import eb
@@ -53,26 +58,30 @@ pdur=pe-ps
 # Centre for plots.
 pa=0.5*(ps+pe)
 
-# Generate phase array.
+# Generate phase array, making sure there is a reasonable amount
+# of baseline available for fitting.
 phi = numpy.linspace(pa-2*pdur, pa+2*pdur, 5000)
 
-# Compute magnitudes.
+# Tell the model to compute magnitudes (author's preference when
+# fitting, can be changed to OBS_LIGHT to compute normalized flux).
 typ = numpy.empty_like(phi, dtype=numpy.uint8)
 typ.fill(eb.OBS_MAG)
 
 # Compute model.
 y = eb.model(parm, phi, typ, eb.FLAG_PHI)
 
+# Are we fitting?
 if do_fit:
   # Make sure we always get the same random deviates.
   numpy.random.seed(42)
 
   # Add noise.
-  nois = 30.0e-6
+  nois = 30.0e-6  # 30 ppm
   
   yobs = y + numpy.random.normal(loc=0.0, scale=nois, size=y.shape)
 
-  # Uncertainties.
+  # Observational uncertainties for fitter.  We'll be nice here and
+  # use the correct answer.
   e_yobs = numpy.empty_like(y, dtype=numpy.double)
   e_yobs.fill(nois)
 
@@ -93,6 +102,8 @@ if do_fit:
   print 'Initial guesses:'
 
   for iparm in tofit:
+    # Decide perturbation.  Perturbing zero won't work too well in
+    # relative so use absolute for those parameters.
     if iparm == eb.PAR_M0:
       pertur=0.1  # absolute
     elif iparm == eb.PAR_PHI0:
@@ -100,10 +111,15 @@ if do_fit:
     else:
       pertur=0.5*parm[iparm]
       
+    # Generate our initial guesses (intentionally bad).
     initval = numpy.random.normal(parm[iparm], scale=pertur)
+
+    # Write out what we're going to use.
     print "{0:<10} {1:14.6f} {2}".format(eb.parnames[iparm],
                                          initval,
                                          eb.parunits[iparm])
+
+    # Set in the problem.
     p.p_value(iparm, parm[iparm], fixed=False)
 
   # Phase seems to confuse the automatic step size choice.
@@ -127,10 +143,14 @@ if do_fit:
   # Model (without weights).
   ymod = eb.model(sol.params, phi, typ, eb.FLAG_PHI)
 
-  # Plot it.
+  # Plot data and fit.
   plt.errorbar(phi, yobs, e_yobs)
   plt.plot(phi, ymod)
 
+# Plot original model.
 plt.plot(phi, y)
+
+# Bright up!
 plt.gca().invert_yaxis()
+
 plt.show()
